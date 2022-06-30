2892fda93229df500b4f0839a04aaa9c0acca181
MagdaGlowacz <100359339+MagdaGlowacz@users.noreply.github.com>
2022-06-30 13:01:14 +0200

    Showing current temperature

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function displayTemerature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTLM = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "6a4efc2b00b6750237f00ef8688bbc7c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemerature);
