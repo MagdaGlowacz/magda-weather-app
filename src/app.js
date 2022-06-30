ced4f7e3ad8827d273f45e90457e59966f6a270b
MagdaGlowacz <100359339+MagdaGlowacz@users.noreply.github.com>
2022-06-30 16:32:06 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,18 @@ function displayTemerature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "6a4efc2b00b6750237f00ef8688bbc7c";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "6a4efc2b00b6750237f00ef8688bbc7c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemerature);
+}
 
-axios.get(apiUrl).then(displayTemerature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
