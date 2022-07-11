18ceeeba030b60621e288aa2256aa85bd0ff9b42
MagdaGlowacz <100359339+MagdaGlowacz@users.noreply.github.com>
2022-07-11 11:02:14 +0200

    Again - clean up of the code

--- src/app.js
+++ src/app.js
@@ -21,37 +21,6 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-forecast.forEach(function (forecastDay, index) {
-  if (index < 6) {
-    forecastHTML =
-      forecastHTML +
-      `
-      <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}° </span>
-        </div>
-      </div>
-  `;
-  }
-});
-
-forecastHTML = forecastHTML + `</div>`;
-forecastElement.innerHTML = forecastHTML;
-console.log();
-
 function displayTemerature(responseforecastHTML) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
